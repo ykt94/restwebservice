1ba9801a6646914a4a321709bc4bd1c5b1554878
AlexanderT <alptyktsk@gmail.com>
2021-08-31 10:55:49 +0900

    lesson passed

---
+++ README.md
@@ -0,0 +1,3 @@
+# restwebservice
+Building a RESTful Web Service - Java Spring
+From https://spring.io/guides/gs/rest-service/
---
+++ src/main/java/com/baken/teach/restwebservice/GreetingController.java
@@ -0,0 +1,17 @@
+package com.baken.teach.restwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+    public static final String template = "Hello, %s!";
+    public final AtomicLong counter = new AtomicLong();
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue = "World") String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
